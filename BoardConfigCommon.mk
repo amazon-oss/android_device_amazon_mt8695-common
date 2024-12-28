70489c2109c04c70c6a71f70e9334378fe4cebb5
R0rt1z2 <me@r0rt1z2.com>
2024-12-28 01:26:20 +0100

    mt8695-common: Set Bootloader and Platform flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,5 +11,13 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_VENDOR := amazon
+TARGET_BOARD_PLATFORM := mt8695
+
 # Inherit the proprietary files
 -include vendor/amazon/mt8695-common/BoardConfigVendor.mk
