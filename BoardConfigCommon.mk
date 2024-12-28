a85bca1b13d4e67ce13a8367faa1edffeb64c1c3
R0rt1z2 <me@r0rt1z2.com>
2024-12-28 01:26:06 +0100

    mt8695-common: Define Architecture flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -3,5 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
 # Inherit the proprietary files
 -include vendor/amazon/mt8695-common/BoardConfigVendor.mk
